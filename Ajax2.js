1235fdbbcd9fdbaf3ecfb59ce2b2f17b386544d5
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-04-25 10:42:37 +0530

    Ajax Practice

--- Ajax2.js
+++ Ajax2.js
@@ -10,8 +10,9 @@ function buttonClickHandler(){
     const xhr = new XMLHttpRequest();
     // Open Request
 
-    xhr.open('POST',"http://dummy.restapiexample.com/api/v1/create",true)  
-    xhr.getResponseHeader('Content-Type' , 'application/json');
+    //xhr.open('POST',"http://dummy.restapiexample.com/api/v1/create",true)
+    xhr.open('GET',"ajaxServer.JSON",true)  
+    //xhr.getResponseHeader('Content-Type' , 'application/json');
     // when response is ready to deliver from server to client
     xhr.onprogress=function(){
         console.log("in progress....");
@@ -26,21 +27,23 @@ function buttonClickHandler(){
             let obj = JSON.parse(this.responseText);
             console.log(obj);
         
-        //let lst = document.getElementById("list");
-        //str="";
-        //for(key in obj.data){
-        //    str+=`<li>$(obj.data[key].employee_name)</li>`;
-        //}
-        //lst.innerHTML = str;
+        let lst = document.getElementById("list");
+        str="";
+        for(key in obj.Employees){
+            if(obj.Employees[key].salary<20000){
+            str+=`<li>`+ (obj.Employees[key].name)+" "+(obj.Employees[key].age) + " "+ (obj.Employees[key].salary) +`</li>`;}
+        }
+        lst.innerHTML = str;
     }
         else{
             console.error("Some Error occured");
         }
     }
+    xhr.send();
     // send the request to the server
-    prmtrs = '{"name":"Ram","salary":"12000","age":"25"}';
-    xhr.send(prmtrs);
-    console.log("work done......!")
+    //prmtrs = '{"name":"Ram","salary":"12000","age":"25"}';
+    //xhr.send(prmtrs);
+    //console.log("work done......!")
 
 
 // Illutration of on ready state change functions
