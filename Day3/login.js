43e0624f2f8050179e8b7b6df0f58987b0b0578e
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-04-27 12:20:36 +0530

    Ajax Practice

--- Day3/login.js
+++ Day3/login.js
@@ -1,9 +1,9 @@
 //----------------------------------------------------------------day 3----------------------------------------------------------------//
 
 let get_btn = document.getElementById("btn1");
-get_btn.addEventListener("click",buttonClickHandler);
+get_btn.addEventListener("click", buttonClickHandler);
 
-function buttonClickHandler(){
+function buttonClickHandler() {
     console.log("you have clicked the Get button");
     // create the instance of XMLHttpRequest Object
 
@@ -11,35 +11,36 @@ function buttonClickHandler(){
     // Open Request
 
     //xhr.open('POST',"http://dummy.restapiexample.com/api/v1/create",true)
-    xhr.open('GET',"loginServer.JSON",true)  
+    xhr.open('GET', "loginServer.JSON", true)
     //xhr.getResponseHeader('Content-Type' , 'application/json');
     // when response is ready to deliver from server to client
-    xhr.onprogress=function(){
+    xhr.onprogress = function () {
         console.log("in progress....");
     }
 
-    xhr.onreadystatechange=function(){
+    xhr.onreadystatechange = function () {
         console.log("ready state is", xhr.readyState);
     }
 
-    xhr.onload = function(){
-        if (this.status=== 200){
+    xhr.onload = function () {
+        if (this.status === 200) {
             let obj = JSON.parse(this.responseText);
             console.log(obj);
-        
-        let input = document.getElementById("u1").value;
-        let pass = document.getElementById("p1").value;
-        for(key in obj.Users){
-            if(obj.Users[key].Username==input && obj.Users[key].Password==pass ){
 
-            console.log("Login is Successful...!!");
-        break;}
-            else{
-                console.log("Login Declined...!!");
+            let input = document.getElementById("u1").value;
+            let pass = document.getElementById("p1").value;
+            for (key in obj.Users) {
+                if (obj.Users[key].Username == input && obj.Users[key].Password == pass) {
+
+                    console.log("Login is Successful...!!");
+                    break;
+                }
+                else {
+                    console.log("Login Declined...!!");
+                }
             }
         }
-    }
-        else{
+        else {
             console.error("Some Error occured");
         }
     }
